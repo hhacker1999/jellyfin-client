9309f417c6a4ff17ee1b584ee2aa05219d492e9e
Harsh Kumar <harshkumar@Harshs-MacBook-Air.local>
2022-05-02 17:16:57 +0530

    remove unused import

--- lib/domain/tmdb/tmdb_facade.dart
+++ lib/domain/tmdb/tmdb_facade.dart
@@ -1,4 +1,3 @@
-import 'package:jellyfin_client/domain/models/video_item/jellyfin_video_item.dart';
 import 'package:jellyfin_client/domain/models/video_item/video_item_model.dart';
 
 abstract class TmdbFacade{
